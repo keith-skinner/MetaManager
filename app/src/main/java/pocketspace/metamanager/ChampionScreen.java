6e01ce032e3a6c572be10c338882dee566fcb94c
Keith Skinner <brian.skinner121@myci.csuci.edu>
2019-03-13 02:46:43 -0700

    going home

--- app/src/main/java/pocketspace/metamanager/ChampionScreen.java
+++ app/src/main/java/pocketspace/metamanager/ChampionScreen.java
@@ -1,5 +1,7 @@
 package pocketspace.metamanager;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -32,27 +34,20 @@ public class ChampionScreen extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     public void OpenBuildSelection()
     {
         Intent intent = new Intent(this, BuildList.class);
         startActivity(intent);
     }
-
-    /*public void addListenerOnButton() {
-        Toast toast = Toast.makeText(getApplicationContext(), "some message", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-        toast.show();
-
-        ImageButton img = findViewById(R.id.jaxBttn);
-        img.setOnClickListener(this::myFancyMethod);
-    }
-
-    public void myFancyMethod(View v) {
-        // does something very interesting
-        LayoutInflater inflator=getLayoutInflater();
-        v = inflator.inflate(R.layout.builds_list_screen, null, false);
-        v.startAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
-        setContentView(v);
-    }*/
 
 }
