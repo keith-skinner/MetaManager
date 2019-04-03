28a67bdb3db04ce3660e479a64ff3b63c9f67bb6
Adam <agcapdeville@gmail.com>
2019-04-03 14:44:05 -0700

    Fragment Character implementing START

--- moba/src/main/java/pocketspace/metamanager/CharacterSelectActivity.java
+++ moba/src/main/java/pocketspace/metamanager/CharacterSelectActivity.java
@@ -1,7 +1,10 @@
 package pocketspace.metamanager;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import pocketspace.metamanager.moba.R;
@@ -20,6 +23,19 @@ public class CharacterSelectActivity extends AppCompatActivity{
         TextView txtView = (TextView)findViewById(R.id.text);
         txtView.setText(value);
 
-        
+        RelativeLayout character = this.findViewById(R.id.character_select_list);
+
+
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.character_placeholder, new FragmentCharacter());
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
+
+
     }
+
+
 }
