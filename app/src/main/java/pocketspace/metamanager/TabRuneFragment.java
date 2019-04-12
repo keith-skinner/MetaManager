03ac96ccd6743a3e99f134daa09034621598365c
Keith Skinner <keithskinner93@gmail.com>
2019-04-12 01:46:43 -0700

    changed secondary runes to be more flexy

--- app/src/main/java/pocketspace/metamanager/TabRuneFragment.java
+++ app/src/main/java/pocketspace/metamanager/TabRuneFragment.java
@@ -1,15 +1,34 @@
 package pocketspace.metamanager;
 
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class TabRuneFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_primary_rune, container, false);
+
+
+    }
+
+    private void makeRuneUnselected(View view)
+    {
+        //make opaque
+        ImageView myImage = (ImageView) view.findViewById(R.id.botRoleButton);
+        myImage.setAlpha(127);
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(0);
+
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+        myImage.setColorFilter(filter);
     }
 }
