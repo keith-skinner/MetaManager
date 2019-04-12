2e65493a6146dd0718f25c676582cf407f467147
Keith Skinner <keithskinner93@gmail.com>
2019-04-11 22:43:38 -0700

    replaced all - with _ in file names for runes in lol module

--- app/src/main/java/pocketspace/metamanager/FragmentRune.java
+++ app/src/main/java/pocketspace/metamanager/FragmentRune.java
@@ -69,7 +69,7 @@ public class FragmentRune extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_rune, container, false);
+        return inflater.inflate(R.layout.fragment_rune, container, false);
     }
 
 
