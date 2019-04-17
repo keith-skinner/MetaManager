6c0079a07a572ecee16df8b4bd093f5d872629a3
Keith Skinner <keithskinner93@gmail.com>
2019-04-16 22:46:22 -0700

    Wait Keith. You thought I'd be ok with you getting Korean food if our app was due?

--- app/src/main/java/pocketspace/metamanager/ActivityAboutPage.java
+++ app/src/main/java/pocketspace/metamanager/ActivityAboutPage.java
@@ -2,6 +2,7 @@ package pocketspace.metamanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ActivityAboutPage extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class ActivityAboutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_content);
+        Log.println(Log.INFO, "metamanager", "Hello World");
     }
 }
