6446a63142b151139507aed01ef8eb4e36d02d3b
Adam <agcapdeville@gmail.com>
2019-04-23 16:15:19 -0700

    organizing

---
+++ app/src/main/java/pocketspace/metamanager/screen_activities/ActivityAboutScreen.java
@@ -0,0 +1,15 @@
+package pocketspace.metamanager.screen_activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pocketspace.metamanager.R;
+
+public class ActivityAboutScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_content);
+    }
+}
---
+++ app/src/main/java/pocketspace/metamanager/tab/TabItemFragment.java
@@ -0,0 +1,6 @@
+package pocketspace.metamanager.tab;
+
+import android.support.v4.app.Fragment;
+
+public class TabItemFragment extends Fragment {
+}
