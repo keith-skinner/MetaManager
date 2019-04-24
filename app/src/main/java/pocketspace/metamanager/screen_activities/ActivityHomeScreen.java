2a092e29392b7ee5055198d5e1dea7a257729739
Adam <agcapdeville@gmail.com>
2019-04-23 22:51:00 -0700

    yo

--- app/src/main/java/pocketspace/metamanager/screen_activities/ActivityHomeScreen.java
+++ app/src/main/java/pocketspace/metamanager/screen_activities/ActivityHomeScreen.java
@@ -10,6 +10,6 @@ public class ActivityHomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.build_screen);
+        setContentView(R.layout.home_screen);
     }
 }
