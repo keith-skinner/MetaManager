c3ff0219bd4391a2e0a94949b156e293cdabaef8
Keith Skinner <keithskinner93@gmail.com>
2019-05-12 00:29:46 -0700

    - MockBuild now gives a proper string for a mock build - Quick fix to Build.role - Made Parser work and cleaned - - Should consider swiching to being a static method and not constructor. - - Should consider using the XmlPullParser factory for instantiation but works for now.

--- app/src/main/java/pocketspace/metamanager/data/build/MockBuild.java
+++ app/src/main/java/pocketspace/metamanager/data/build/MockBuild.java
@@ -72,79 +72,54 @@ public class MockBuild {
         }
     }
 
-    public String makeBuildXML() {
-        return
-                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "\n" +
-                "<build buildName=\"dick\" character=\"Aatrox\" role=\"Top\">\n" +
-                "\n" +
-                "    <primary family=\"precision\">\n" +
-                "        <keystone>0</keystone>\n" +
+    public static String makeBuildXML() {
+        return "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n" +
+                "<build name=\"A new build\" champion=\"Aatrox\" role=\"TOP\">\n" +
+                "    <primary family=\"PRECISION\">\n" +
+                "        <keystone>1</keystone>\n" +
                 "        <rune>1</rune>\n" +
-                "        <rune>2</rune>\n" +
                 "        <rune>0</rune>\n" +
-                "    </primary>\n" +
-                "\n" +
-                "    <secondary family=\"domination\">\n" +
-                "        <rune>1</rune>\n" +
                 "        <rune>2</rune>\n" +
+                "    </primary>\n" +
+                "    <secondary family=\"INSPIRATION\">\n" +
+                "        <rune>1</rune>\n" +
+                "        <rune>0</rune>\n" +
                 "        <rune>-1</rune>\n" +
                 "    </secondary>\n" +
-                "\n" +
                 "    <tertiary>\n" +
                 "        <rune>1</rune>\n" +
-                "        <rune>1</rune>\n" +
-                "        <rune>1</rune>\n" +
+                "        <rune>0</rune>\n" +
+                "        <rune>2</rune>\n" +
                 "    </tertiary>\n" +
-                "\n" +
                 "    <summoners>\n" +
-                "        <spell name=\"flash\"/>\n" +
-                "        <spell name=\"teleport\"/>\n" +
+                "        <spell name=\"FLASH\"/>\n" +
+                "        <spell name=\"TELEPORT\"/>\n" +
                 "    </summoners>\n" +
-                "\n" +
-                "    <starting>\n" +
-                "        <block name=\"OFFENSIVE START\">\n" +
-                "           <item name=\"dorans_sheild\" quantity=\"3\"/>\n" +
-                "           <item name=\"cloth_armour\" quantity=\"1\"/>\n" +
-                "        </block>\n"+
-                "        <block name=\"DEFENSIVE START\">\n" +
-                "           <item name=\"dorans_sheild\" quantity=\"3\"/>\n" +
-                "           <item name=\"cloth_armour\" quantity=\"1\"/>\n" +
-                "        </block>\n"+
-                "    </starting>\n" +
-                "\n" +
-                "    <core>\n" +
-                "        <item name=\"trinity_force\"/>\n" +
-                "    </core>\n" +
-                "\n" +
-                "    <situational>\n" +
-                "        <block name=\"Heavy AP\">\n" +
-                "            <item name=\"spirit_visage\"/>\n" +
-                "            <item name=\"abyssal_mask\"/>\n" +
-                "        </block>\n" +
-                "    </situational>\n" +
-                "\n" +
                 "    <skills>\n" +
-                "        <skill>Q</skill> <!-- level 1 -->\n" +
-                "        <skill>W</skill> <!-- level 2 -->\n" +
-                "        <skill>E</skill> <!-- level 3 -->\n" +
-                "        <skill>Q</skill> <!-- level 4 -->\n" +
-                "        <skill>Q</skill> <!-- level 5 -->\n" +
-                "        <skill>R</skill> <!-- level 6 -->\n" +
-                "        <skill>E</skill> <!-- level 7 -->\n" +
-                "        <skill>W</skill> <!-- level 8 -->\n" +
-                "        <skill>W</skill> <!-- level 9 -->\n" +
-                "        <skill>W</skill> <!-- level 10 -->\n" +
-                "        <skill>W</skill> <!-- level 11 -->\n" +
-                "        <skill>W</skill> <!-- level 12 -->\n" +
-                "        <skill>W</skill> <!-- level 13 -->\n" +
-                "        <skill>W</skill> <!-- level 14 -->\n" +
-                "        <skill>W</skill> <!-- level 15 -->\n" +
-                "        <skill>W</skill> <!-- level 16 -->\n" +
-                "        <skill>W</skill> <!-- level 17 -->\n" +
-                "        <skill>W</skill> <!-- level 18 -->\n" +
+                "        <skill>Q</skill><skill>W</skill><skill>E</skill>\n" +
+                "        <skill>Q</skill><skill>W</skill><skill>E</skill>\n" +
+                "        <skill>Q</skill><skill>W</skill><skill>E</skill>\n" +
+                "        <skill>Q</skill><skill>W</skill><skill>E</skill>\n" +
+                "        <skill>Q</skill><skill>W</skill><skill>E</skill>\n" +
+                "        <skill>R</skill><skill>R</skill><skill>R</skill>\n" +
                 "    </skills>\n" +
-                "\n" +
-                "</build>";
+                "    <items>\n" +
+                "        <block name=\"Core\">\n" +
+                "            <item name=\"Big Damage Item 1\"/>\n" +
+                "            <item name=\"Big Damage Item 2\"/>\n" +
+                "            <item name=\"Big Damage Item 3\"/>\n" +
+                "        </block>\n" +
+                "        <block name=\"Starting\">\n" +
+                "            <item name=\"Starting Item\"/>\n" +
+                "            <item name=\"Health Potion\" quantity=\"2\"/>\n" +
+                "            <item name=\"Trinket\"/>\n" +
+                "        </block>\n" +
+                "        <block name=\"Situational\">\n" +
+                "            <item name=\"Tanky Item 1\"/>\n" +
+                "            <item name=\"Tanky Item 2\"/>\n" +
+                "            <item name=\"Tanky Item 3\"/>\n" +
+                "        </block>\n" +
+                "    </items>\n" +
+                "</build>\n";
     }
 }
--- app/src/main/java/pocketspace/metamanager/data/build/serialize/BuildParser.java
+++ app/src/main/java/pocketspace/metamanager/data/build/serialize/BuildParser.java
@@ -8,348 +8,219 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 
 
 import pocketspace.metamanager.data.build.Build;
+import pocketspace.metamanager.data.build.item.Item;
 import pocketspace.metamanager.data.build.item.ItemBlock;
+import pocketspace.metamanager.data.build.skill.Skill;
 
 
-public class BuildParser {
-
+public class BuildParser
+{
     public Build build = new Build();
-
     private static final String ns = null;
 
+    //parent tag
+    private static final String BUILD_TAG = "build";
 
+    //depth 1 tags
+    private static final String PRIMARY_TAG = "primary";
+    private static final String SECONDARY_TAG = "secondary";
+    private static final String TERTIARY_TAG = "tertiary";
+    private static final String SUMMONERS_TAG = "summoners";
+    private static final String SKILLS_TAG = "skills";
+    private static final String ITEMS_TAG = "items";
 
-    public BuildParser(InputStream inStream) throws XmlPullParserException, IOException
+    //depth 2 tags
+    private static final String KEYSTONE_TAG = "keystone";
+    private static final String SPELL_TAG = "spell";
+    private static final String SKILL_TAG = "skill";
+    private static final String RUNE_TAG = "rune";
+    private static final String BLOCK_TAG = "block";
+
+    //depth 3 tags
+    private static final String ITEM_TAG = "item";
+
+    //attributes
+    private static final String NAME_ATTR = "name";
+    private static final String CHAMPION_ATTR = "champion";
+    private static final String ROLE_ATTR = "role";
+    private static final String FAMILY_ATTR = "family";
+    private static final String QUANTITY_ATTR = "quantity";
+
+    public BuildParser(InputStream inStream) throws IOException
     {
         try {
             XmlPullParser parser = Xml.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             parser.setInput(inStream, null);
             parser.nextTag();
-
-            readFeed(parser);
-
+            parseBuild(parser);
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
         } finally {
-            inStream.close();
+            if (inStream != null)
+                inStream.close();
         }
     }
 
-    // by nested section
-    private void readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-
-
-        parser.require(XmlPullParser.START_TAG, ns, "build");
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-
-            String name = parser.getName();
-
-            switch (name) {
-                case "primary":
-                    readPrimary(parser);
-                    break;
-                case "secondary":
-                    readSecondary(parser);
-                    break;
-                case "tertiary":
-                    readTertiary(parser);
-                    break;
-                case "summoners":
-                    readSummoners(parser);
-                    break;
-                case "starting":
-                    readStarting(parser);
-                    break;
-                case "skills":
-                    readSkills(parser);
-                    break;
-                default:
-                    skip(parser);
-                    break;
-            }
+    private void parseTopLevelBuild(XmlPullParser parser) throws XmlPullParserException, IOException
+    {
+        switch (parser.getName()) {
+            case "primary":
+                readPrimary(parser);
+                break;
+            case "secondary":
+                readSecondary(parser);
+                break;
+            case "tertiary":
+                readTertiary(parser);
+                break;
+            case "summoners":
+                readSummoners(parser);
+                break;
+            case "skills":
+                readSkills(parser);
+                break;
+            case "items":
+                readItems(parser);
+                break;
+            default:
+                break;
         }
+    }
+
+    private void parseBuild(XmlPullParser parser) throws XmlPullParserException, IOException
+    {
+        parser.require(XmlPullParser.START_TAG, ns, BUILD_TAG);
+        build.name = parser.getAttributeValue(ns, NAME_ATTR);
+        build.champion = parser.getAttributeValue(ns, CHAMPION_ATTR);
+        build.role.setRole(parser.getAttributeValue(ns, ROLE_ATTR));
+        while (parser.nextTag() != XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() == XmlPullParser.END_TAG)
+                if (parser.getName().equals(BUILD_TAG))
+                    break;
+            if (parser.getEventType() != XmlPullParser.START_TAG)
+                continue;
+            parseTopLevelBuild(parser);
+        }
+        parser.require(XmlPullParser.END_TAG, ns, BUILD_TAG);
+    }
+
+    private static int readKeyStone(XmlPullParser parser) throws  IOException, XmlPullParserException
+    {
+        parser.nextTag();
+        parser.require(XmlPullParser.START_TAG, ns, KEYSTONE_TAG);
+        int keystone = Integer.parseInt(parser.nextText());
+        parser.require(XmlPullParser.END_TAG, ns, KEYSTONE_TAG);
+        return keystone;
+    }
+
+    private static int readRune(XmlPullParser parser) throws  IOException, XmlPullParserException {
+        parser.nextTag();
+        parser.require(XmlPullParser.START_TAG, ns, RUNE_TAG);
+        int rune = Integer.parseInt(parser.nextText());
+        parser.require(XmlPullParser.END_TAG, ns, RUNE_TAG);
+        return rune;
     }
 
     private void readPrimary(XmlPullParser parser) throws  IOException, XmlPullParserException
     {
-        parser.require(XmlPullParser.START_TAG, ns, "primary");
-
-        Log.d("clear",":primary START");
-        build.runes.primary.family.setFamily(parser.getAttributeValue(ns, "family"));
-
-        int runeCount = 0;
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("keystone")) {
-                parser.require(XmlPullParser.START_TAG, ns, "keystone");
-                build.runes.primary.keystone = Integer.parseInt(parser.nextText());
-                parser.require(XmlPullParser.END_TAG, ns, "keystone");
-            }
-            if (parser.getName().equals("rune")) {
-                runeCount++;
-                parser.require(XmlPullParser.START_TAG, ns, "rune");
-                switch (runeCount){
-                    case 1:
-                        build.runes.primary.row1 = Integer.parseInt(parser.nextText());
-                        break;
-                    case 2:
-                        build.runes.primary.row2 = Integer.parseInt(parser.nextText());
-                        break;
-                    case 3:
-                        build.runes.primary.row3 = Integer.parseInt(parser.nextText());
-                        break;
-                }
-                parser.require(XmlPullParser.END_TAG, ns, "rune");
-            }
-        }
-        Log.d("clear",":primary END");
-
+        parser.require(XmlPullParser.START_TAG, ns, PRIMARY_TAG);
+        build.runes.primary.family.setFamily(parser.getAttributeValue(ns, FAMILY_ATTR));
+        build.runes.primary.keystone = readKeyStone(parser);
+        build.runes.primary.row1 = readRune(parser);
+        build.runes.primary.row2 = readRune(parser);
+        build.runes.primary.row3 = readRune(parser);
+        parser.nextTag();
+        parser.require(XmlPullParser.END_TAG, ns, PRIMARY_TAG);
     }
 
     private void readSecondary(XmlPullParser parser) throws  IOException, XmlPullParserException
     {
-        Log.d("clear",":secondary START");
-        parser.require(XmlPullParser.START_TAG, ns, "secondary");
-        int runeCount = 0;
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("rune")) {
-                runeCount++;
-                parser.require(XmlPullParser.START_TAG, ns, "rune");
-                switch (runeCount){
-                    case 1:
-                        build.runes.secondary.row1 = Integer.parseInt(parser.nextText());
-                        break;
-                    case 2:
-                        build.runes.secondary.row2 = Integer.parseInt(parser.nextText());
-                        break;
-                    case 3:
-                        build.runes.secondary.row3 = Integer.parseInt(parser.nextText());
-                        break;
-                }
-                parser.require(XmlPullParser.END_TAG, ns, "rune");
-            }
-        }
-        Log.d("clear",":secondary END");
+        parser.require(XmlPullParser.START_TAG, ns, SECONDARY_TAG);
+        build.runes.secondary.family.setFamily(parser.getAttributeValue(ns, FAMILY_ATTR));
+        build.runes.secondary.row1 = readRune(parser);
+        build.runes.secondary.row2 = readRune(parser);
+        build.runes.secondary.row3 = readRune(parser);
+        parser.nextTag();
+        parser.require(XmlPullParser.END_TAG, ns, SECONDARY_TAG);
     }
 
     private void readTertiary(XmlPullParser parser) throws  IOException, XmlPullParserException
     {
-        Log.d("clear",":tertiary START");
-        parser.require(XmlPullParser.START_TAG, ns, "tertiary");
-        int runeCount = 0;
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("rune")) {
-                runeCount++;
-                parser.require(XmlPullParser.START_TAG, ns, "rune");
-                switch (runeCount){
-                    case 1:
-                        build.runes.tertiary.row1 = Integer.parseInt(parser.nextText());
-                        break;
-                    case 2:
-                        build.runes.tertiary.row2 = Integer.parseInt(parser.nextText());
-                        break;
-                    case 3:
-                        build.runes.tertiary.row3 = Integer.parseInt(parser.nextText());
-                        break;
-                }
-                parser.require(XmlPullParser.END_TAG, ns, "rune");
-            }
-        }
-        Log.d("clear",":tertiary END");
+        parser.require(XmlPullParser.START_TAG, ns, TERTIARY_TAG);
+        build.runes.tertiary.row1 = readRune(parser);
+        build.runes.tertiary.row2 = readRune(parser);
+        build.runes.tertiary.row3 = readRune(parser);
+        parser.nextTag();
+        parser.require(XmlPullParser.END_TAG, ns, TERTIARY_TAG);
+    }
 
+    private static String readSpell(XmlPullParser parser) throws IOException, XmlPullParserException
+    {
+        parser.nextTag();
+        parser.require(XmlPullParser.START_TAG, ns, SPELL_TAG);
+        String summoner = parser.getAttributeValue(ns, NAME_ATTR);
+        parser.nextTag();
+        parser.require(XmlPullParser.END_TAG, ns, SPELL_TAG);
+        return summoner;
     }
 
     private void readSummoners(XmlPullParser parser) throws IOException, XmlPullParserException
     {
-        Log.d("clear",":summoners START");
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("spell"))
-            {
-                parser.require(XmlPullParser.START_TAG, ns, "spell");
-                build.summoner1.setSummoner(parser.getAttributeValue(ns, "name"));
-                //Log.i("clear", ":  spell: " + build.summoners.summoner1);
-                parser.nextTag();
-                parser.require(XmlPullParser.END_TAG, ns, "spell");
+        parser.require(XmlPullParser.START_TAG, ns, SUMMONERS_TAG);
+        build.summoner1.setSummoner(readSpell(parser));
+        build.summoner2.setSummoner(readSpell(parser));
+        parser.nextTag();
+        parser.require(XmlPullParser.END_TAG, ns, SUMMONERS_TAG);
+    }
 
-                parser.nextTag();
-
-                parser.require(XmlPullParser.START_TAG, ns, "spell");
-                //build.summoners.summoner2 = parser.getAttributeValue(ns, "name");
-                //Log.i("clear", ":  spell: " + build.summoners.summoner2);
-                parser.nextTag();
-                parser.require(XmlPullParser.END_TAG, ns, "spell");
-            }
-        }
-        Log.d("clear",":summoners END");
+    private Item readItem(XmlPullParser parser) throws IOException, XmlPullParserException
+    {
+        parser.require(XmlPullParser.START_TAG, ns, ITEM_TAG);
+        Item item = new Item (parser.getAttributeValue(ns, NAME_ATTR));
+        if (parser.getAttributeCount() == 2)
+            item.setQuantity(Integer.parseInt(parser.getAttributeValue(ns, QUANTITY_ATTR)));
+        parser.nextTag();
+        parser.require(XmlPullParser.END_TAG, ns, ITEM_TAG);
+        return item;
     }
 
     private ItemBlock readBlock(XmlPullParser parser) throws IOException, XmlPullParserException
     {
-        Log.d("clear",":block START");
-
-        //build.block = new Build.ItemGroup();
-        //build.block.items = new ArrayList<>();
-        //build.block.itemQuantities  = new ArrayList<>();
-
-        //build.block.name = parser.getAttributeValue(ns, "name");
-        parser.nextTag(); // skip block start.. and go until block
-        String name = parser.getName();
-        while (name.compareTo("block") != 0)
-        {
-            if(parser.getName().equals("item"))
-            {
-                String quantity;
-                if(parser.getAttributeCount() > 1){
-                    quantity = parser.getAttributeValue(ns, "quantity");
-                }else {
-                    quantity = "1";
-                }
-                String item = parser.getAttributeValue(ns, "name");
-                //build.block.items.add(item);
-                //build.block.itemQuantities.add(Integer.parseInt(quantity));
-                parser.nextTag(); // skip endTag
-            }
-            parser.nextTag();
-            name = parser.getName();
-        }
-        Log.d("clear",":block END");
-
-        //return build.block;
-        return null;
+        parser.require(XmlPullParser.START_TAG, ns, BLOCK_TAG);
+        ItemBlock block = new ItemBlock(parser.getAttributeValue(ns, NAME_ATTR));
+        while (parser.nextTag() != XmlPullParser.END_TAG)
+            block.add(readItem(parser));
+        parser.require(XmlPullParser.END_TAG, ns, BLOCK_TAG);
+        return block;
     }
 
-    private void readStarting(XmlPullParser parser) throws IOException, XmlPullParserException
+    private void readItems(XmlPullParser parser) throws IOException, XmlPullParserException
     {
-        Log.d("clear",":starting START");
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("block")) {
-                //Build.ItemGroup newBlock;
-                //newBlock = readBlock(parser);
-                //build.items.starting.add(newBlock);
-            }
-        }
-        Log.d("clear",":starting END");
+        parser.require(XmlPullParser.START_TAG, ns, ITEMS_TAG);
+        while (parser.nextTag() != XmlPullParser.END_TAG)
+            build.items.add(readBlock(parser));
+        parser.require(XmlPullParser.END_TAG, ns, ITEMS_TAG);
     }
 
-    private void readCore(XmlPullParser parser) throws IOException, XmlPullParserException
+    private Skill readSkill(XmlPullParser parser) throws IOException, XmlPullParserException
     {
-        Log.d("clear",":core START");
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("block")) {
-                //Build.ItemGroup newBlock;
-                //newBlock = readBlock(parser);
-                //build.items.core.add(newBlock);
-            }
-        }
-        Log.d("clear",":core END");
-    }
-
-    private void readSituational(XmlPullParser parser) throws IOException, XmlPullParserException
-    {
-        Log.d("clear",":situational START");
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("block")) {
-                //Build.ItemGroup newBlock;
-                //newBlock = readBlock(parser);
-                //build.items.situational.add(newBlock);
-            }
-        }
-        Log.d("clear",":situational END");
-    }
-
-    // for debugging parse.attr
-    private void printParserAttr(XmlPullParser parser, Integer index, String name) throws  IOException, XmlPullParserException
-    {
-        Log.d("clear", ":  attributes: "+parser.getAttributeCount() );
-        Log.d("clear", ":  :  Name: " + parser.getAttributeName(index));
-        Log.d("clear", ":  :  Value: " + parser.getAttributeValue(index));
-        Log.d("clear", ":  "+name+": = " + parser.getAttributeValue(ns, name));
+        parser.nextTag();
+        parser.require(XmlPullParser.START_TAG, ns, SKILL_TAG);
+        Skill skill = new Skill(parser.nextText());
+        parser.require(XmlPullParser.END_TAG, ns, SKILL_TAG);
+        return skill;
     }
 
     private void readSkills(XmlPullParser parser) throws IOException, XmlPullParserException
     {
-        Log.d("clear",":skills START");
-        parser.require(XmlPullParser.START_TAG, ns, "skills");
-        while (parser.next() != XmlPullParser.END_TAG)
-        {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("skill")) {
-                parser.require(XmlPullParser.START_TAG, ns, "skill");
-                char c = parser.nextText().charAt(0);
-                //Build.Skill s = build.charToSkill(c);
-                //build.skills.add(s);
-                parser.require(XmlPullParser.END_TAG, ns, "skill");
-            }
-        }
-        Log.d("clear",":skills END");
+        parser.require(XmlPullParser.START_TAG, ns, SKILLS_TAG);
+        for (int i=0; i<18; ++i)
+            build.skills.add(readSkill(parser));
+        parser.nextTag();
+        parser.require(XmlPullParser.END_TAG, ns, SKILLS_TAG);
     }
-
-
-    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
-        String result = "";
-        if (parser.next() == XmlPullParser.TEXT) {
-            result = parser.getText();
-            parser.nextTag();
-        }
-        return result;
-    }
-
-
-    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException
-    {
-        if (parser.getEventType() != XmlPullParser.START_TAG)
-        {
-            throw new IllegalStateException();
-        }
-        int depth = 1;
-        while (depth != 0)
-        {
-            switch (parser.next())
-            {
-                case XmlPullParser.END_TAG:
-                    depth--;
-                    break;
-                case XmlPullParser.START_TAG:
-                    depth++;
-                    break;
-            }
-        }
-    }
-
 }
