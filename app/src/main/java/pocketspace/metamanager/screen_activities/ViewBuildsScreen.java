322e6e8498e264c23f4edd3e5b6cba25f7114e7d
abdulwahab-aden <abduwahab.aden618@myci.csuci.edu>
2019-05-13 04:55:19 -0700

    Added search bar that appears/disappears to view_builds_screen.xml page. Changed search bar color to black and text inside to white, which looks nicer in my opinion.
    Also, made the build cards rounded again. We can change the color later if we want, but I set that value to currently be the light shade of purple we have in the colors.xml file for splashBackgroundColor.

--- app/src/main/java/pocketspace/metamanager/screen_activities/ViewBuildsScreen.java
+++ app/src/main/java/pocketspace/metamanager/screen_activities/ViewBuildsScreen.java
@@ -1,9 +1,13 @@
 package pocketspace.metamanager.screen_activities;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParserException;
@@ -17,11 +21,16 @@ import java.io.InputStream;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+
 import pocketspace.metamanager.R;
 import pocketspace.metamanager.data.build.serialize.BuildParser;
 
 
 public class ViewBuildsScreen extends AppCompatActivity {
+
+    LinearLayout lnr_expanded_search;
+    View topLineView;
+    ImageView iv_search ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +54,32 @@ public class ViewBuildsScreen extends AppCompatActivity {
 
         //END TEST
 
+        topLineView = this.findViewById(R.id.view_topLine);
+        lnr_expanded_search = this.findViewById(R.id.lnr_expanded_search);
+        iv_search = this.findViewById(R.id.iv_search_minimized);
+        iv_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (lnr_expanded_search.getVisibility() == View.GONE)
+                    iv_search.setVisibility(View.GONE);
+                    topLineView.setVisibility(View.GONE);
+                    lnr_expanded_search.setVisibility(View.VISIBLE);
+            }
+        });
+
         CardView cardView = this.findViewById(R.id.build_card_0);
         cardView.setOnClickListener(view -> {
             Intent intent = new Intent(this, BuildScreen.class);
             view.getContext().startActivity(intent);
         });
     }
-    
+
 
     // For creating a new file...
-    public void createFile(File directory, String fname, String buildContent)
-    {
+    public void createFile(File directory, String fname, String buildContent) {
 
-        if(!directory.exists() && !directory.mkdirs()){
-            Log.e("clear","Could not make directory");
+        if (!directory.exists() && !directory.mkdirs()) {
+            Log.e("clear", "Could not make directory");
         }
 
         try {
@@ -67,8 +88,8 @@ public class ViewBuildsScreen extends AppCompatActivity {
             writer.append(buildContent);
             writer.flush();
             writer.close();
-        } catch (Exception e){
-            Log.d("createFile","fail:"+e);
+        } catch (Exception e) {
+            Log.d("createFile", "fail:" + e);
             e.printStackTrace();
         }
     }
@@ -77,4 +98,11 @@ public class ViewBuildsScreen extends AppCompatActivity {
         return new File(this.getFilesDir(), "/builds");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        lnr_expanded_search.setVisibility(View.GONE);
+        iv_search.setVisibility(View.VISIBLE);
+        topLineView.setVisibility(View.VISIBLE);
+    }
 }
